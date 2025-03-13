241af6c01fea78b637d31f7683cdaff9c47b1198
knorcross27 <norcrosk@union.edu>
2025-03-13 00:57:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 
 #include "toaster_alert_system.h"
 #include "user_interface.h"
+
 //=====[Main function, the program entry point after power on or reset]========
 
+//runs initialization functions
+// updates display
+// loops over update functions
 int main()
 {
     toasterAlertSystemInit();
